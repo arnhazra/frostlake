f4bebce8e56235165446e73fc4ec453f6afdf687
Arnab Hazra <arnhazra@gmail.com>
2022-11-23 23:59:13 +0530

    autodeploy

--- view/src/index.tsx
+++ view/src/index.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'node-snackbar/dist/snackbar.min.css'
-import './Style.sass'
+import './styles/main.sass'
 import App from './App'
 
 //React Render
